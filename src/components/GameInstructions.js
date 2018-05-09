6b55025ce7d84e8253fb88bd7d7dc1e91e5e29d6
Russell Schmidt <russell.schmidt@me.com>
2018-05-09 11:18:33 -0700

    add instructions clarity

--- src/components/GameInstructions.js
+++ src/components/GameInstructions.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import Faker from 'faker'
 
 export default () => (
   <div className="game-instructions__text">
-    <h3>Hello. Welcome aboard.</h3>
-    <p>We were impressed with your qualifications and are excited to work with you.</p>
-    <p>As you may know, your task will be to decipher the words that appear on screen.</p>
-    <p>We will provide you with feedback on your performance. </p>
+    <h3>Hello there!</h3>
+    <p>I am {Faker.name.firstName()} {Faker.name.lastName()}, and I am a {Faker.name.jobTitle()}. </p>
+    <p>We were impressed with your qualifications and are excited to work with you on an interim contract basis through a third party agency. Welcome to the team!</p>
+    <p>As you may know, your task will be to <b>decipher the words that appear on screen. Accuracy is crucial to our operations and success.</b></p>
+    <p>We will provide you with feedback on your performance. Various managers will also appear to check in on you.</p>
+    <p>Good luck and see you around the water cooler!</p>
   </div>
 )
